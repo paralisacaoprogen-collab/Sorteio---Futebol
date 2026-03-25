63a2db8f56e6426e23b0b366911829be5355f67c
paralisacaoprogen-collab <paralisacaoprogen@gmail.com>
2026-03-25 14:51:34 -0300

    Add files via upload

---
+++ bash.bash
@@ -0,0 +1,6 @@
+git clone https://github.com/SEU-USUARIO/sorteio-futebol.git
+cd sorteio-futebol
+# copie o index.html para esta pasta
+git add .
+git commit -m "Primeira versão"
+git push origin main
